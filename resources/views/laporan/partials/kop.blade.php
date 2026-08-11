8bbfbbfc8023345a0efd2a7d48c5db0622ca26b6
Lambace <alheemtj@gmail.com>
2026-08-11 13:42:01 +0800

    Kop PDF: pakai kop_nama_sekolah dengan fallback ke nama_sekolah

--- resources/views/laporan/partials/kop.blade.php
+++ resources/views/laporan/partials/kop.blade.php
@@ -1,14 +1,42 @@
 <div class="kop">
+    {{-- Logo INSTANSI (kiri) --}}
     <div class="kop-logo">
-        @if($logo)<img src="{{ $logo }}" alt="Logo">@endif
+        @if($logoInstansi ?? null)
+            <img src="{{ $logoInstansi }}" alt="Logo Instansi">
+        @endif
     </div>
+
+    {{-- Teks tengah --}}
     <div class="kop-teks">
-        <div class="kop-pemerintah">PEMERINTAH PROVINSI SULAWESI TENGGARA</div>
-        <div class="kop-nama">{{ $pengaturan->nama_sekolah ?? 'SMK NEGERI 2 KOLAKA' }}</div>
+        <div class="kop-baris1">{{ $pengaturan->kop_baris1 ?? 'PEMERINTAH PROVINSI SULAWESI TENGGARA' }}</div>
+        <div class="kop-baris2">{{ $pengaturan->kop_baris2 ?? 'DINAS PENDIDIKAN DAN KEBUDAYAAN' }}</div>
+
+        {{-- ===== NAMA SEKOLAH KOP (prioritas: kop_nama_sekolah > nama_sekolah > default) ===== --}}
+        <div class="kop-nama">
+            {{ strtoupper(
+                $pengaturan->kop_nama_sekolah
+                    ?: ($pengaturan->nama_sekolah ?? 'SEKOLAH MENENGAH KEJURUAN (SMK) NEGERI 2 KOLAKA')
+            ) }}
+        </div>
+
+        <div class="kop-alamat">{{ $pengaturan->alamat ?? 'Jln. Poros Kolaka - Pomalaa KM. 16 Kec. Baula Kab. Kolaka Provinsi SULTRA' }}</div>
         <div class="kop-alamat">
-            {{ $pengaturan->alamat ?? 'Jl. Poros Kolaka–Pomalaa KM.16, Kec. Baula, Kab. Kolaka, Sulawesi Tenggara' }}
-            @if($pengaturan->telepon ?? null) &bull; Telp: {{ $pengaturan->telepon }} @endif
-            @if($pengaturan->email ?? null) &bull; Email: {{ $pengaturan->email }} @endif
+            E-mail <span class="kop-link">{{ $pengaturan->email ?? 'smknsatubaula@yahoo.co.id' }}</span>
+            &nbsp;&nbsp;Telp. {{ $pengaturan->telepon ?? '082346999111' }}
+        </div>
+        <div class="kop-alamat">
+            Website: <span class="kop-link">{{ $pengaturan->website ?? 'www.smk1baula.sch.id' }}</span>
+            &nbsp;&nbsp;Server: <span class="kop-link">{{ $pengaturan->server ?? 'sisfo.smk1baula.sch.id' }}</span>
         </div>
     </div>
+
+    {{-- Logo SEKOLAH (kanan) --}}
+    <div class="kop-logo">
+        @if($logo ?? null)
+            <img src="{{ $logo }}" alt="Logo Sekolah">
+        @endif
+    </div>
 </div>
+
+{{-- Double line bawah kop --}}
+<div class="kop-garis"></div>
